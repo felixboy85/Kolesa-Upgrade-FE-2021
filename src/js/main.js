a1082721aec3b8e16e67b4d21f8609dc5ca5cca3
Felix Spivak <felixboy85@gmail.com>
2021-09-24 16:48:20 +0500

    Add some JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
-import '../css/style.css';
+// import '../css/style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello, Felix!!!</h1>
-`;
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello, Felix!!!</h1>
+// `;
+
+const link = document.querySelector('.main__link');
+const close = document.querySelector('.close__btn');
+const overlay = document.querySelector('.overlay');
+
+link.addEventListener('click', () => {
+    overlay.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+});
